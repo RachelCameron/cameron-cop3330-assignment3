642437607aa23e12eb286e3f1487c1da1c9f11e5
Rachel Cameron <rachleecameron@gmail.com>
2021-10-06 15:49:03 -0400

    Initial commit

---
+++ src/main/java/ex44/App.java
@@ -0,0 +1,14 @@
+package ex44;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
