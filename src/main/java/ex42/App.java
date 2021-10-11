ae6db4e7e69650c460ca59c7c55f6140285d5920
Rachel Cameron <rachleecameron@gmail.com>
2021-10-11 13:07:46 -0400

    ex42

--- src/main/java/ex42/App.java
+++ src/main/java/ex42/App.java
@@ -5,10 +5,55 @@ package ex42;
  *  Copyright 2021 Rachel Cameron
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+/**
+ * Exercise 42.
+ *
+ * Reads data from file and displays it as a table.
+ */
 
 public class App {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        try {
+            ArrayList<Person> people = getRecords("src/main/java/ex42/exercise42_input.txt");
+            printTable(people);
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find the file!");
+        }
+    }
+
+    public static Person personFromRecord(String record)  {
+        String[] data = record.split(",");
+        return new Person(data[1], data[0], Double.parseDouble(data[2]));
+    }
+
+    public static ArrayList<Person> getRecords(String filename) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(filename));
+
+        ArrayList<Person> people = new ArrayList<>();
+
+        while (input.hasNextLine())  {
+            Person person = personFromRecord(input.nextLine());
+            people.add(person);
+        }
+
+        input.close();
+
+        return people;
+    }
+
+    public static void printTable(ArrayList<Person> people)  {
+        String f = "%-12s%-12s%-12s%n";
+        String f2 = "%-12s%-12s%-12.0f%n";
+        System.out.printf(f, "Last", "First", "Salary");
+        System.out.println("------------------------------");
+        for (Person person: people) {
+            System.out.format(f2, person.getLast(),
+                    person.getFirst(), person.getSalary());
+        }
     }
 }
