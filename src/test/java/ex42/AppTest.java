ee4d210c67ffa289c790226484c3d82f4af3b7b7
Rachel Cameron <rachleecameron@gmail.com>
2021-10-11 13:08:28 -0400

    ex42 test

--- src/test/java/ex42/AppTest.java
+++ src/test/java/ex42/AppTest.java
@@ -1,4 +1,24 @@
 package ex42;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Exercise 42.
+ */
+
 public class AppTest {
+    @Test
+    public void testParsing()  {
+        String s = "Ling,Mai,55900";
+        Person person = App.personFromRecord(s);
+        assertEquals("Ling", person.getLast());
+        assertEquals("Mai", person.getFirst());
+        assertEquals(55900, person.getSalary());
+    }
 }
