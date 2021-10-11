ab76829d7c42101a032ed41093f27d56022eac50
Rachel Cameron <rachleecameron@gmail.com>
2021-10-11 13:08:39 -0400

    ex43 test

--- src/test/java/ex43/AppTest.java
+++ src/test/java/ex43/AppTest.java
@@ -1,4 +1,30 @@
 package ex43;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+import org.junit.jupiter.api.Test;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Exercise 43.
+ */
+
 public class AppTest {
+    @Test
+    public void testWebsite()  {
+        String title = "Great Title";
+        String author = "Best Author";
+
+        String website = App.generateWebsite(title, author, false, false);
+        assertTrue(website.contains("<title>" + title + "</title>"));
+        assertTrue(website.contains("<meta name=\"author\" content=\"" +
+                author + "\">"));
+
+        assertTrue(Files.exists(Path.of("website/" + title)));
+    }
 }
